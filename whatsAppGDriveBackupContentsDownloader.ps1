195072b24324cc3314ddd7b5b973826105b1ebbd
Banee Ishaque K <baneeishaque@gmail.com>
2023-02-19 18:59:53 +0530

    Create whatsAppGDriveBackupContentsDownloader.ps1

---
+++ whatsAppGDriveBackupContentsDownloader.ps1
@@ -0,0 +1,13 @@
+cd C:\Lab_Data\whapa
+.\.venv\Scripts\Activate.ps1
+git checkout hajara
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\"
+git checkout banee
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\"
+git checkout ismail
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\"
+git checkout jusaira
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\"
+git checkout nasaru
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\"
+git checkout master
