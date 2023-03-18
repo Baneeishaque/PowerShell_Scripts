82feb90b41cd3078082024e95496ad53793121a9
Banee Ishaque K <baneeishaque@gmail.com>
2023-03-18 22:09:30 +0530

    Create ideaUltimateLatestCheckVersionScript.ps1

---
+++ ideaUltimateLatestCheckVersionScript.ps1
@@ -0,0 +1,18 @@
+$latestRelease = Invoke-RestMethod -Uri "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&platform=zip&type=release"
+$latestRc = Invoke-RestMethod -Uri "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&platform=zip&type=rc"
+$latestEap = Invoke-RestMethod -Uri "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&platform=zip&type=eap"
+
+if (([System.Version]$latestRelease.IIU.build -gt [System.Version]$latestRc.IIU.build) -and ([System.Version]$latestRelease.IIU.build -gt [System.Version]$latestEap.IIU.build)) {
+    $latestRelease = $latestRelease.IIU.version
+    $latestBuildNumber = $latestRelease.IIU.build
+}
+elseif (([System.Version]$latestRc.IIU.build -gt [System.Version]$latestRelease.IIU.build) -and ([System.Version]$latestRc.IIU.build -gt [System.Version]$latestEap.IIU.build)) {
+    $latestRelease = $latestRc.IIU.build
+    $latestBuildNumber = $latestRelease
+}
+else {
+    $latestRelease = $latestEap.IIU.build
+    $latestBuildNumber = $latestRelease
+}
+
+Write-Output "$latestBuildNumber $latestRelease"
