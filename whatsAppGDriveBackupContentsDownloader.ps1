d3aed883f8c5ae8667d233ab9709cfc948019c5e
Banee Ishaque K <baneeishaque@gmail.com>
2023-03-25 15:11:41 +0530

    typo
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- whatsAppGDriveBackupContentsDownloader.ps1
+++ whatsAppGDriveBackupContentsDownloader.ps1
@@ -1,13 +1,13 @@
 Set-Location C:\Lab_Data\whapa
 .\.venv\Scripts\Activate.ps1
 git checkout hajara
-python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\" --no_parallel
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout banee
-python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\" --no_parallel
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout ismail
-python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\" --no_parallel
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout jusaira
-python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\" --no_parallel
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout nasaru
-python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Backup Contents\\" --no_parallel
+python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout master
