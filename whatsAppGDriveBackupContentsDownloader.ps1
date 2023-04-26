d8b22af34f06a002e0f2bb45ebb51a9a2b81498b
Banee Ishaque K <baneeishaque@gmail.com>
2023-04-26 13:09:09 +0530

    Update whatsAppGDriveBackupContentsDownloader.ps1

--- whatsAppGDriveBackupContentsDownloader.ps1
+++ whatsAppGDriveBackupContentsDownloader.ps1
@@ -1,5 +1,6 @@
 Set-Location C:\Lab_Data\whapa
 .\.venv\Scripts\Activate.ps1
+
 git checkout hajara
 python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout banee
@@ -11,3 +12,5 @@ python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsAp
 git checkout nasaru
 python "C:\Lab_Data\whapa\libs\whagodri.py" -s -o "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents\\" --no_parallel
 git checkout master
+
+C:\Users\dk\scoop\apps\rclone-beta\current\rclone.exe --config C:\Lab_Data\configurations-private\rclone.conf check "C:\Lab_Data\To_Upload\WhatsApp Last Backup Contents" "Blomp-Banee-Gmail-Drive:WhatsApp Backup Contents" --transfers 4 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --verbose --stats 1s --stats-file-name-length 0 --differ C:\Temp_Data\Avita-WhatsApp-Backup-Contents-differ.txt --missing-on-dst C:\Temp_Data\Avita-WhatsApp-Backup-Contents-Missing-On-Cloud.txt
