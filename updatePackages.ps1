40b6a70da415099c768a89d46b707a1deac4478b
Banee Ishaque K <baneeishaque@gmail.com>
2023-04-29 20:02:29 +0530

    Windows Store Apps

--- updatePackages.ps1
+++ updatePackages.ps1
@@ -2,3 +2,4 @@ winget upgrade --include-unknown
 scoop update
 scoop status
 choco outdated
+Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
