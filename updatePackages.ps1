252747f5ae6c398d485c507597701403e9888d75
Banee Ishaque K <baneeishaque@gmail.com>
2023-05-06 18:23:15 +0530

    typo
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- updatePackages.ps1
+++ updatePackages.ps1
@@ -1,5 +1,15 @@
+Write-Output 'Winget Outdated Apps'
+Write-Output '------------------------------'
 winget upgrade --include-unknown
+Write-Output 'Scoop Outdated Apps'
+Write-Output '------------------------------'
 scoop update
 scoop status
+Write-Output 'Chocolatey Outdated Apps'
+Write-Output '------------------------------'
 choco outdated
+Write-Output 'Update Windows Store Apps'
+Write-Output '------------------------------'
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+Write-Output 'After Scoop Update, Clean Scoop Cache'
+Write-Output 'After Chocolatey Update, Clean Chocolatey Cache'
