62464f0f3d0febfeb7100767fb5e4b7818002f6d
Banee Ishaque K <baneeishaque@gmail.com>
2023-06-07 17:34:04 +0530

    Flutter master branch update
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- updatePackages.ps1
+++ updatePackages.ps1
@@ -1,15 +1,25 @@
 Write-Output 'Winget Outdated Apps'
 Write-Output '------------------------------'
 winget upgrade --include-unknown
+
 Write-Output 'Scoop Outdated Apps'
 Write-Output '------------------------------'
 scoop update
 scoop status
+
 Write-Output 'Chocolatey Outdated Apps'
 Write-Output '------------------------------'
 choco outdated
+
 Write-Output 'Update Windows Store Apps'
 Write-Output '------------------------------'
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
+
 Write-Output 'After Scoop Update, Clean Scoop Cache'
 Write-Output 'After Chocolatey Update, Clean Chocolatey Cache'
+
+Write-Output 'Flutter master branch update'
+Set-Location "$(scoop prefix fvm)\versions\master"
+git pull
+Set-Location C:\Lab_Data\Account_Ledger_Windows_Flutter
+fvm flutter build --debug windows
