ab2c96db5df947f69b8c9775e3fd12c1c3660871
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-12 22:27:55 +0530

    Revert "Including Visual Studio core editor"
    This reverts commit 235d165cc46154e54a08fdaac76db62a652c855d.

--- generateScoopManifestsForVisualStudioWorkloads.ps1
+++ generateScoopManifestsForVisualStudioWorkloads.ps1
@@ -2,7 +2,7 @@ $latestVersion = Select-String -InputObject $(Invoke-WebRequest -Uri 'https://le
 
 $fileHash = (Get-FileHash -InputStream (([System.Net.WebClient]::new()).OpenRead('https://aka.ms/vs/17/pre/vs_enterprise.exe')) -Algorithm SHA256).Hash.ToLower()
 
-$(Select-String -InputObject $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MicrosoftDocs/visualstudio-docs/main/docs/install/includes/vs-2022/workload-component-id-vs-enterprise.md").Content -Pattern "## (.+)\s\s\*\*ID:\*\* Microsoft\.VisualStudio\.Workload\.(.+)\s\s\*\*Description:\*\* (.+)" -AllMatches | ForEach-Object { $_.Matches }) | ForEach-Object {
+$(Select-String -InputObject $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MicrosoftDocs/visualstudio-docs/main/docs/install/includes/vs-2022/workload-component-id-vs-enterprise.md").Content -Pattern "## (.+)\s\s\*\*ID:\*\* Microsoft\.VisualStudio\.Workload\.(.+)\s\s\*\*Description:\*\* (.+)" -AllMatches | ForEach-Object { $_.Matches }) | Where-Object { $_.Groups[2].Value -notmatch "CoreEditor" } | ForEach-Object {
 
     # Write-Output $_.Groups[1].Value $_.Groups[2].Value $_.Groups[3].Value
 
