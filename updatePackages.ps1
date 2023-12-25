e7f2046aa996827096961c1737a997abd2d25037
Banee Ishaque K <baneeishaque@gmail.com>
2023-12-25 16:31:52 +0530

    Update updatePackages.ps1

--- updatePackages.ps1
+++ updatePackages.ps1
@@ -1,6 +1,6 @@
 Write-Output 'Winget Outdated Apps'
 Write-Output '------------------------------'
-winget upgrade --include-unknown
+winget upgrade --include-unknown --include-pinned --verbose-logs --disable-interactivity
 
 Write-Output 'Scoop Outdated Apps'
 Write-Output '------------------------------'
@@ -9,21 +9,46 @@ scoop status
 
 Write-Output 'Chocolatey Outdated Apps'
 Write-Output '------------------------------'
-choco outdated
+# choco outdated --debug --verbose --trace --accept-license --confirm --prerelease
+choco outdated --accept-license --confirm --prerelease
 
 Write-Output 'Update Windows Store Apps'
 Write-Output '------------------------------'
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
 
-Write-Output 'After Scoop Update, Clean Scoop Cache'
+Write-Output 'After Scoop Update, Clean Scoop Apps & Cache'
 Write-Output 'After Chocolatey Update, Clean Chocolatey Cache'
 
 Write-Output 'Flutter master branch update'
 Set-Location "$(scoop prefix fvm)\versions\master"
 git pull
+
+Write-Output 'Buildign flutter sample apps'
+$tempLocation = 'C:\Flutter-Temp-Lab'
+$tempAppName = 'my_app'
+$tempModuleName = 'my_module'
 jabba use openjdk@21.0.1
-Set-Location C:\Lab_Data\Account_Ledger_Windows_Flutter\account_ledger_lib_kotlin_native
-.\compileMainCFile.ps1
-fvm spawn master clean
-fvm spawn master build windows --debug
+New-Item -Path $tempLocation -ItemType Directory
+Set-Location $tempLocation
+fvm spawn master create $tempAppName
+Set-Location $tempAppName
+fvm spawn master build bundle
+fvm spawn master build apk
+fvm spawn master build appbundle
 fvm spawn master build windows
+fvm spawn master build web
+fvm spawn master build bundle --debug
+fvm spawn master build apk --debug
+fvm spawn master build appbundle --debug
+fvm spawn master build windows --debug
+Set-Location ..
+Remove-Item $tempAppName -Recurse
+fvm spawn master create $tempModuleName --template=module
+Set-Location $tempModuleName
+fvm spawn master build aar
+fvm spawn master build aar --debug
+Set-Location ..
+Remove-Item $tempModuleName -Recurse
+Set-Location ..
+Remove-Item $tempLocation -Recurse
+Set-Location $PSScriptRoot
