4c3ed09edfc9cc8d7fb40b825547589f4ff75f7a
Banee Ishaque K <baneeishaque@gmail.com>
2024-02-17 01:53:36 +0530

    CallSave-ModuleInfo.ps1 in updatePackages.ps1
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- PowerShell_Modules_Scripts/CallSave-ModuleInfo.ps1
+++ PowerShell_Modules_Scripts/CallSave-ModuleInfo.ps1
@@ -1,8 +1,10 @@
+$scriptDirectory = Split-Path -Parent $PSCommandPath
+
 # Dot source the script file that contains the function
-. ".\Save-ModuleInfo.ps1"
+. "$scriptDirectory\Save-ModuleInfo.ps1"
 
 # Now you can call the function
-Save-ModuleInfo
+# Save-ModuleInfo
 
 # Or call the function with a specific path
 Save-ModuleInfo -path "C:\Lab_Data\configurations-private\PowerShell"
--- updatePackages.ps1
+++ updatePackages.ps1
@@ -2,6 +2,10 @@ Write-Output 'Sqlyog Backup Jobs'
 Write-Output '------------------------------'
 Start-Process -FilePath 'C:\Program Files\SQLyog Trial\SJA.exe' -ArgumentList '"C:\Lab_Data\Account-Ledger-Server\db_backup_jobs\nomadller_hostinger_temp_Avita-Windows.xml" -l"C:\Users\dk\AppData\Roaming\SQLyog\sja.log" -s"C:\Users\dk\AppData\Roaming\SQLyog\sjasession.xml"' -Wait
 
+Write-Output 'PowerShell Modules Backup'
+Write-Output '------------------------------'
+. '.\PowerShell_Modules_Scripts\CallSave-ModuleInfo.ps1'
+
 Write-Output 'Optimize Scoop Cache'
 Write-Output '------------------------------'
 . '.\Scoop_Scripts\Optimize-ScoopCache.ps1'
