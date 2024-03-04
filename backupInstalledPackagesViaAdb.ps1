f249042d63e6682ef25768f50e7c266124d2ac2c
Banee Ishaque K <baneeishaque@gmail.com>
2024-03-05 03:12:43 +0530

    multiple devices on backupInstalledPackagesViaAdb.ps1
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- backupInstalledPackagesViaAdb.ps1
+++ backupInstalledPackagesViaAdb.ps1
@@ -4,8 +4,8 @@ function Get-EmulatorInstanceName {
     $deviceToInstanceMap = @{
         'G011A'  = 'Android 7 DIGI KGB Hajara Banee Gmail 1-2-3 MEmu'
         'A5010'  = 'Android 7 DIGI KGB Ismail MEmu'
-        'emulator-5554' = 'Android 11 BlueStacks App Player'
-        'emulator-5584' = 'Android 11 Banee Gmail BlueStacks App Player'
+        'b0qxxx' = 'Android 11 BlueStacks App Player'
+        'r0qxxx' = 'Android 11 Banee Gmail BlueStacks App Player'
     }
 
     $instanceName = $deviceToInstanceMap[$deviceInfo]
@@ -18,42 +18,48 @@ function Get-EmulatorInstanceName {
     }
 }
 
-$deviceInfo = (adb devices -l | Select-String 'device product:').ToString() -replace '.*device product:(.+?)\s+model:.+?\s+device:.+?\s+transport_id:\d+', '$1'
-$emulatorInstanceName = Get-EmulatorInstanceName -deviceInfo $deviceInfo
+$deviceInfos = (adb devices -l | Select-String 'device product:')
 
-function Get-PackageInfo {
-    param([string]$packageName)
-    $packageInfo = adb shell dumpsys package $packageName
-    $versionName = ($packageInfo | Select-String 'versionName=').ToString() -replace 'versionName=', '' -replace '\s+', ''
-    $firstInstallTime = ($packageInfo | Select-String 'firstInstallTime=').ToString() -replace 'firstInstallTime=', '' -replace '^\s+|\s+$', '' -replace '\s+(\d+:\d+:\d+)', ' $1'
-    $lastUpdateTime = ($packageInfo | Select-String 'lastUpdateTime=').ToString() -replace 'lastUpdateTime=', '' -replace '^\s+|\s+$', '' -replace '\s+(\d+:\d+:\d+)', ' $1'
-    $installerPackageNameLine = $packageInfo | Select-String 'installerPackageName='
-    $installerPackageName = if ($installerPackageNameLine) { $installerPackageNameLine.ToString() -replace 'installerPackageName=', '' -replace '\s+', '' } else { 'Not Available' }
-    "$packageName,$versionName,$firstInstallTime,$lastUpdateTime,$installerPackageName"
-}
+foreach ($deviceInfo in $deviceInfos) {
 
-$packageList = (adb shell pm list packages -3) -notmatch 'com.android.' | Sort-Object
-$csvRows = foreach ($package in $packageList) { $packageName = $package -replace 'package:', '' -replace '\s+', ''; Get-PackageInfo -packageName $packageName }
-$timestamp = Get-Date -Format 'MM-dd-yyyy HH-mm-ss'
+    $deviceId = $deviceId = ($deviceInfo -replace '(\w+)\s+device.*', '$1')
+    $deviceInfo = $deviceInfo -replace '.*device product:(.+?)\s+model:.+?\s+device:.+?\s+transport_id:\d+', '$1'
+    $emulatorInstanceName = Get-EmulatorInstanceName -deviceInfo $deviceInfo
 
-$outputFileNameCurrentFolder = "${emulatorInstanceName} Packages ${timestamp}.csv"
-$csvRows | Out-File -FilePath $outputFileNameCurrentFolder -Encoding UTF8
-Write-Host "CSV file '$outputFileNameCurrentFolder' created in the current folder."
+    function Get-PackageInfo {
+        param([string]$packageName)
+        $packageInfo = adb -s $deviceId shell dumpsys package $packageName
+        $versionName = ($packageInfo | Select-String 'versionName=').ToString() -replace 'versionName=', '' -replace '\s+', ''
+        $firstInstallTime = ($packageInfo | Select-String 'firstInstallTime=').ToString() -replace 'firstInstallTime=', '' -replace '^\s+|\s+$', '' -replace '\s+(\d+:\d+:\d+)', ' $1'
+        $lastUpdateTime = ($packageInfo | Select-String 'lastUpdateTime=').ToString() -replace 'lastUpdateTime=', '' -replace '^\s+|\s+$', '' -replace '\s+(\d+:\d+:\d+)', ' $1'
+        $installerPackageNameLine = $packageInfo | Select-String 'installerPackageName='
+        $installerPackageName = if ($installerPackageNameLine) { $installerPackageNameLine.ToString() -replace 'installerPackageName=', '' -replace '\s+', '' } else { 'Not Available' }
+        "$packageName,$versionName,$firstInstallTime,$lastUpdateTime,$installerPackageName"
+    }
 
-$outputFolder1 = 'C:\Lab_Data\configurations-private'
-$outputFileNameFolder1 = Join-Path $outputFolder1 "${emulatorInstanceName} Packages.csv"
-$csvRows | Out-File -FilePath $outputFileNameFolder1 -Encoding UTF8
-Write-Host "CSV file '$outputFileNameFolder1' created in folder '$outputFolder1'."
+    $packageList = (adb -s $deviceId shell pm list packages -3) -notmatch 'com.android.' | Sort-Object
+    $csvRows = foreach ($package in $packageList) { $packageName = $package -replace 'package:', '' -replace '\s+', ''; Get-PackageInfo -packageName $packageName }
+    $timestamp = Get-Date -Format 'MM-dd-yyyy HH-mm-ss'
 
-if ($emulatorInstanceName -match 'MEmu') {
-    $outputFolder2 = "C:\Lab_Data\Memu-Virtual-Appliances\$emulatorInstanceName"
-    $outputFileNameFolder2 = Join-Path $outputFolder2 "${emulatorInstanceName} Packages.csv"
-    $csvRows | Out-File -FilePath $outputFileNameFolder2 -Encoding UTF8
-    Write-Host "CSV file '$outputFileNameFolder2' created in folder '$outputFolder2'."
-}
-elseif ($emulatorInstanceName -match 'BlueStacks') {
-    $outputFolder2 = 'C:\Lab_Data\BlueStacks-Backups'
-    $outputFileNameFolder2 = Join-Path $outputFolder2 "${emulatorInstanceName} Packages.csv"
-    $csvRows | Out-File -FilePath $outputFileNameFolder2 -Encoding UTF8
-    Write-Host "CSV file '$outputFileNameFolder2' created in folder '$outputFolder2'."
+    $outputFileNameCurrentFolder = "${emulatorInstanceName} Packages ${timestamp}.csv"
+    $csvRows | Out-File -FilePath $outputFileNameCurrentFolder -Encoding UTF8
+    Write-Host "CSV file '$outputFileNameCurrentFolder' created in the current folder."
+
+    $outputFolder1 = 'C:\Lab_Data\configurations-private'
+    $outputFileNameFolder1 = Join-Path $outputFolder1 "${emulatorInstanceName} Packages.csv"
+    $csvRows | Out-File -FilePath $outputFileNameFolder1 -Encoding UTF8
+    Write-Host "CSV file '$outputFileNameFolder1' created in folder '$outputFolder1'."
+
+    if ($emulatorInstanceName -match 'MEmu') {
+        $outputFolder2 = "C:\Lab_Data\Memu-Virtual-Appliances\$emulatorInstanceName"
+        $outputFileNameFolder2 = Join-Path $outputFolder2 "${emulatorInstanceName} Packages.csv"
+        $csvRows | Out-File -FilePath $outputFileNameFolder2 -Encoding UTF8
+        Write-Host "CSV file '$outputFileNameFolder2' created in folder '$outputFolder2'."
+    }
+    elseif ($emulatorInstanceName -match 'BlueStacks') {
+        $outputFolder2 = 'C:\Lab_Data\BlueStacks-Backups'
+        $outputFileNameFolder2 = Join-Path $outputFolder2 "${emulatorInstanceName} Packages.csv"
+        $csvRows | Out-File -FilePath $outputFileNameFolder2 -Encoding UTF8
+        Write-Host "CSV file '$outputFileNameFolder2' created in folder '$outputFolder2'."
+    }
 }
