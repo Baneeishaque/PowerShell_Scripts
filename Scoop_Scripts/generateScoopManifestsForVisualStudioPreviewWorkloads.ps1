2d394cb06b5121195ae1ddf998043000da06c51f
Banee Ishaque K <baneeishaque@gmail.com>
2024-03-13 13:28:18 +0530

    generateScoopManifestsForVisualStudioWorkloads
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- Scoop_Scripts/generateScoopManifestsForVisualStudioPreviewWorkloads.ps1
+++ Scoop_Scripts/generateScoopManifestsForVisualStudioPreviewWorkloads.ps1
@@ -1,6 +1,6 @@
-$latestVersion = $(Select-String -InputObject $(Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes').Content -Pattern '>Visual Studio 2022 version ([\d.]+)<').Matches.Groups[1].Value
+$latestVersion = Select-String -InputObject $(Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes-preview').Content -Pattern 'Visual Studio 2022 version ((?<major>[\d.]+) Preview (?<minor>\d\d?))' | ForEach-Object -Process { "$($_.Matches.Groups[2]).$($_.Matches.Groups[3])"}
 
-$fileHash = (Get-FileHash -InputStream (([System.Net.WebClient]::new()).OpenRead('https://aka.ms/vs/17/release/vs_enterprise.exe')) -Algorithm SHA256).Hash.ToLower()
+$fileHash = (Get-FileHash -InputStream (([System.Net.WebClient]::new()).OpenRead('https://aka.ms/vs/17/pre/vs_enterprise.exe')) -Algorithm SHA256).Hash.ToLower()
 
 $(Select-String -InputObject $(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MicrosoftDocs/visualstudio-docs/main/docs/install/includes/vs-2022/workload-component-id-vs-enterprise.md").Content -Pattern "## (.+)\s\s\*\*ID:\*\* Microsoft\.VisualStudio\.Workload\.(.+)\s\s\*\*Description:\*\* (.+)" -AllMatches | ForEach-Object { $_.Matches }) | ForEach-Object {
 
@@ -10,12 +10,12 @@ $(Select-String -InputObject $(Invoke-WebRequest -Uri "https://raw.githubusercon
     # Write-Output "visual-studio-2022-preview-enterprise-$($_.Groups[2].Value.ToLower())-recommended-installer.json"
     # Write-Output "visual-studio-2022-preview-enterprise-$($_.Groups[2].Value.ToLower())-full-installer.json"
 
-    $destinationPrefix = "Scoop Manifests\visual-studio-2022-enterprise-$($_.Groups[2].Value.ToLower())-"
+    $destinationPrefix = "Scoop Manifests\visual-studio-2022-preview-enterprise-$($_.Groups[2].Value.ToLower())-"
     $destinationSuffix = "installer.json"
     $destination = "$destinationPrefix$destinationSuffix"
 
     New-Item -ItemType File -Path $destination -Force
-    Copy-Item "visual-studio-2022-enterprise-workload-installer.json.template" -Destination $destination
+    Copy-Item "visual-studio-2022-preview-enterprise-workload-installer.json.template" -Destination $destination
 
     ((Get-Content -path $destination -Raw) -replace '{latestVersion}', $latestVersion) | Set-Content -Path $destination
     ((Get-Content -path $destination -Raw) -replace '{fileHash}', $fileHash) | Set-Content -Path $destination
