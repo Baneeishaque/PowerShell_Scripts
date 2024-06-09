9953849349fe691f1774af19bf04539db592652a
Banee Ishaque K <Baneeishaque@gmail.com>
2024-06-09 16:10:16 +0530

    Get-FileHash -> Get-ExecutableHash
    Signed-off-by: Banee Ishaque K <Baneeishaque@gmail.com>

--- Scoop_Scripts/Scoop App Manifest Generation Scripts/Visual Studio Scoop App Manifest Generation Scripts/generateScoopManifestsForVisualStudioVariantWorkloads.ps1
+++ Scoop_Scripts/Scoop App Manifest Generation Scripts/Visual Studio Scoop App Manifest Generation Scripts/generateScoopManifestsForVisualStudioVariantWorkloads.ps1
@@ -1,4 +1,4 @@
-function Get-FileHash($url)
+function Get-ExecutableHash($url)
 {
     return (Get-FileHash -InputStream (([System.Net.WebClient]::new()).OpenRead($url)) -Algorithm SHA256).Hash.ToLower()
 }
@@ -39,12 +39,12 @@ function Update-ScoopManifests()
 
 # Generate manifests for Visual Studio
 $latestVersion = $( Select-String -InputObject $( Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes' ).Content -Pattern '>Visual Studio 2022 version ([\d.]+)<' ).Matches.Groups[1].Value
-$fileHash = Get-FileHash -url 'https://aka.ms/vs/17/release/vs_enterprise.exe'
+$fileHash = Get-ExecutableHash -url 'https://aka.ms/vs/17/release/vs_enterprise.exe'
 New-Manifests -latestVersion $latestVersion -fileHash $fileHash -varient ""
 
 # Generate manifests for Visual Studio Preview
 $latestVersion = Select-String -InputObject $( Invoke-WebRequest -Uri 'https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-notes-preview' ).Content -Pattern 'Visual Studio 2022 version ((?<major>[\d.]+) Preview (?<minor>\d\d?))' | ForEach-Object -Process { "$( $_.Matches.Groups[2] ).$( $_.Matches.Groups[3] )" }
-$fileHash = Get-FileHash -url 'https://aka.ms/vs/17/pre/vs_enterprise.exe'
+$fileHash = Get-ExecutableHash -url 'https://aka.ms/vs/17/pre/vs_enterprise.exe'
 New-Manifests -latestVersion $latestVersion -fileHash $fileHash -varient "-preview"
 
 Update-ScoopManifests
