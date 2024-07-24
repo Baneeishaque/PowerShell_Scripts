05ea28844b99227541cd838b220408fd5940bc28
Banee Ishaque K <baneeishaque@gmail.com>
2024-07-24 19:27:09 +0530

    Get-FileExtensionsAndNoExtensionsForGitLfs: Get-FileExtensionsAndNoExtensions O/P array handller

--- Get-FileExtensionsAndNoExtensionsForGitLfs.ps1
+++ Get-FileExtensionsAndNoExtensionsForGitLfs.ps1
@@ -2,11 +2,14 @@ $scriptDirectory = Split-Path -Parent $PSCommandPath
 
 . "$scriptDirectory\Get-FileExtensionsAndNoExtensions.ps1"
 
-# Get all file extensions in the directory using your function
-$dirExtensions = Get-FileExtensionsAndNoExtensions | ForEach-Object { $_.TrimStart('.') }
-# $dirExtensions
+$dirExtensionsWithNoExtensionFiles = Get-FileExtensionsAndNoExtensions
+#$dirExtensionsWithNoExtensionFiles
+
+$dirExtensions = $dirExtensionsWithNoExtensionFiles[0] | ForEach-Object { $_.TrimStart('.') }
+#$dirExtensions
 
 # Get extensions from .gitattributes
+# TODO: Filter already present no extension files too.
 $gitAttrExtensions = Get-Content .\.gitattributes | ForEach-Object {if ($_ -match "\*\.(.+) filter") {$matches[1]}}
 
 # Define a list of common extensions
@@ -18,5 +21,7 @@ $combinedExtensions = $commonExtensions + $gitAttrExtensions
 # Filter the dirExtensions list to only include those not present in combinedExtensions
 $filteredDirExtensions = $dirExtensions | Where-Object {$combinedExtensions -notcontains $_}
 
+$filteredDirExtensionsWithNoExtensionFiles = $filteredDirExtensions + $dirExtensionsWithNoExtensionFiles[1]
+
 # Output the filtered list
-$filteredDirExtensions
+$filteredDirExtensionsWithNoExtensionFiles
