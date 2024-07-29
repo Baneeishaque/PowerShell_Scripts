1b303ff8ab2962d7644534e2c65a9d90feef3b08
Banee Ishaque K <baneeishaque@gmail.com>
2024-07-30 02:01:53 +0530

    Optimize-Scoopcache.ps1: Clean

--- Scoop_Scripts/Optimize-ScoopCache.ps1
+++ Scoop_Scripts/Optimize-ScoopCache.ps1
@@ -31,7 +31,7 @@ function Optimize-ScoopCache {
     foreach ($entry in $sortedHashTable) {
 
         # Get the scoop search output for the app
-        $searchOutput = & "$(scoop prefix scoop)\bin\scoop.ps1" search $entry.Key 2>$null
+        $searchOutput = & "$(scoop prefix scoop)\bin\scoop.ps1" search $entry.Key
 
         # Filter the search output for objects where the name is exactly the same as the app name
         $filteredSearchOutput = $searchOutput | Where-Object { $_.Name -eq $entry.Key }
