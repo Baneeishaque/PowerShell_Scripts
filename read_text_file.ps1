6c9a04d5d339062b45e262a776e01d3a4160593c
Banee Ishaque K <Baneeishaque@gmail.com>
2025-08-28 01:34:41 +0530

    Removes trailing whitespace
    Improves code cleanliness and consistency by removing unnecessary trailing whitespace characters.
    
    Signed-off-by: Banee Ishaque K <Baneeishaque@gmail.com>

--- read_text_file.ps1
+++ read_text_file.ps1
@@ -1,7 +1,7 @@
 $result="SELECT * FROM ``ticket`` WHERE ``agent`` IN ("
 $i=0
 foreach($line in Get-Content '.\read text file sample.txt') {
-    
+
     # if($line -match $regex){
         # Work here
     # }
@@ -11,7 +11,7 @@ foreach($line in Get-Content '.\read text file sample.txt') {
         $result="$result'$line'"
     }
     else {
-        $result="$result,'$line'"        
+        $result="$result,'$line'"
     }
     # Write-Host $line
 
